d2fffe60059ff475f35a4c63d91f6fdcc6eb01e8
Amit Kumar <amit.kumar@novanet.net>
2017-11-22 11:25:36 +0530

    Added new file GameListTableViewController to to show user either playing or waiting to play

---
+++ Tic-Tac-Toe/GameListTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameListTableViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Amit on 22/11/17.
+//  Copyright © 2017 Amit. All rights reserved.
+//
+
+import UIKit
+
+class GameListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
