29bb63a0014b8ed4eacf63942586f5ea55c1316a
Amit Kumar <amit.kumar@novanet.net>
2017-11-23 17:43:06 +0530

    Implemented registerUser(Name name : String) -> Void method to register new user

--- Tic-Tac-Toe/Database.swift
+++ Tic-Tac-Toe/Database.swift
@@ -19,8 +19,30 @@ class ServerData {
     var otherDevicesTokens : [String?] = []
     var otherPlayer : [Player?] = []
     
-    private func registerUser(){
+    //MARK: Action
+    private func registerUser(Name name : String){
+        let token = Messaging.messaging().fcmToken
+        rootRef = Database.database().reference()
+        ref = Database.database().reference(withPath : "Games")
         
+        
+        // MARK : Accessing json child
+        let gameName = rootRef.child("Games\(token!)")
+        let gameId = gameName.child("GameId")
+        let fcmTocken = gameName.child("fcmtoken")
+        let player = gameName.child("player")
+        let playerid = player.child("playerId")
+        let playername = player.child("name")
+        let playersign = player.child("Sign")
+        let newplayer = Player(name,.circle)
+        
+        
+        //MARK : setting values
+        gameId.setValue("1")
+        fcmTocken.setValue(token)
+        playerid.setValue(newplayer.id)
+        playername.setValue(newplayer.name)
+        playersign.setValue(String(describing: newplayer.sign))
     }
     
     func getUserData() -> UserData {
